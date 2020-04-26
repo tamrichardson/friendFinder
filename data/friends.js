5c4e33f2d3a3989c40241ee53567c385754e47f6
Tammy Richardson <atrich0220@gmail.com>
2020-04-26 14:05:41 -0600

    added data folder and friends.js

---
+++ data/friends.js
@@ -0,0 +1,85 @@
+var friendsArray = [
+    {
+        "name": "Thor",
+        "photo": "https://i.pinimg.com/736x/7b/ae/1a/7bae1a624afec920179401bbe9bbdc33.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Ironman",
+        "photo": "https://i.insider.com/577d2929dd0895d15e8b45a1?width=1100&format=jpeg&auto=webp",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Star Lord",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTNKG3XNCsJroIE9buQvyOf-05cFefvYpLICm3f7Tg7em-w0I9w&usqp=CAU",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Black Widow",
+        "photo": "https://cnet1.cbsistatic.com/img/ITvNQ8Fe7NBf0J5dP-o4FSKTnU8=/940x0/2019/10/01/31f06410-a845-4fd3-a866-bc3714ee5f5f/black2.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Captian Marval",
+        "photo": "https://cdn.images.express.co.uk/img/dynamic/36/750x445/1205437.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.  
+module.exports = friendsArray;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
+var friendsArray = require("../data/tableData");
 var waitListData = require("../data/waitinglistData");
 
 
