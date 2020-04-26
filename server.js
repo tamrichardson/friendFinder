38aa51d6ae920895b49cc847d562a9d554b91204
Tammy Richardson <atrich0220@gmail.com>
2020-04-26 12:29:12 -0600

    initial files

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+// Create express app instance.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// LISTENER
+p.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
